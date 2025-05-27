8e9c203ee97d6f232dbe66bd91c61b608b87c996
Nico Munasatya <46341663+nicomunasatya@users.noreply.github.com>
2025-05-27 13:56:57 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,8 @@
-
+const fs = require('fs');
+const path = require('path');
+const { ethers, JsonRpcProvider } = require('ethers');
+const axios = require('axios');
+const moment = require('moment-timezone');
+const readline = require('readline');
+const TelegramBot = require('node-telegram-bot-api');
+require('dotenv').config();
