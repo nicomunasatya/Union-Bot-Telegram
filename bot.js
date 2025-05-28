27b8a4ca25d874bca97cd41b3e624656f2dd0ac1
Nico Munasatya <46341663+nicomunasatya@users.noreply.github.com>
2025-05-28 13:11:56 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,3 +6,19 @@ const moment = require('moment-timezone');
 const readline = require('readline');
 const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
+
+// Setting logger
+const logger = {
+  info: (msg) => console.log(`[✓] ${msg}`),
+  warn: (msg) => console.log(`[⚠] ${msg}`),
+  error: (msg) => console.log(`[✗] ${msg}`),
+  success: (msg) => console.log(`[✅] ${msg}`),
+  loading: (msg) => console.log(`[⟳] ${msg}`),
+  step: (msg) => console.log(`[➤] ${msg}`),
+  banner: () => {
+    console.log(`---------------------------------------------`);
+    console.log(`  Union Testnet Auto Bot - Script by airdropnode (https://t.me/airdrop_node)  `);
+    console.log(`---------------------------------------------`);
+    console.log();
+  }
+};
