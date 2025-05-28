8d605e5434bae659eda83d210760d848ed063aba
Nico Munasatya <46341663+nicomunasatya@users.noreply.github.com>
2025-05-28 13:12:29 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,3 +22,29 @@ const logger = {
     console.log();
   }
 };
+
+// ABI contract
+const UCS03_ABI = [
+  {
+    inputs: [
+      { internalType: 'uint32', name: 'channelId', type: 'uint32' },
+      { internalType: 'uint64', name: 'timeoutHeight', type: 'uint64' },
+      { internalType: 'uint64', name: 'timeoutTimestamp', type: 'uint64' },
+      { internalType: 'bytes32', name: 'salt', type: 'bytes32' },
+      {
+        components: [
+          { internalType: 'uint8', name: 'version', type: 'uint8' },
+          { internalType: 'uint8', name: 'opcode', type: 'uint8' },
+          { internalType: 'bytes', name: 'operand', type: 'bytes' },
+        ],
+        internalType: 'struct Instruction',
+        name: 'instruction',
+        type: 'tuple',
+      },
+    ],
+    name: 'send',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+];
